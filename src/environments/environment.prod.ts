efcdc43bc8ee1fdd731c3913eead2f805af6edd9
caovthang <49051611+caovthang@users.noreply.github.com>
2022-05-05 22:12:06 +0700

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://accf3a077a0f945bd965140667bca1ae-1211737112.us-west-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
